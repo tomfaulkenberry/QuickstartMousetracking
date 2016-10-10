985abe03f6ef76aab159a8cdcd200fd2ce1df763
Thomas J. Faulkenberry <faulkenberry@tarleton.edu>
2016-10-10 16:22:54 -0500

    edited analysis script

--- results/analysis.R
+++ results/analysis.R
@@ -8,19 +8,19 @@ library(ggplot2)
 setwd("~/github/mousetrackingOperatorPreviewEffect/results/")
 source("helperFunctions.R")
 
-summaryMeasures <- read.csv("processed.csv")
+summaryMeasures <- read.csv("processed2.csv")
 xNorm <- read.csv("nx.csv")
 yNorm <- read.csv("ny.csv")
 
-rawData <- cbind(summaryMeasures,xNorm,yNorm)
+rawData <- summaryMeasures
 
 # note: 16 trials manually removed before processing..total of 51*256=13056 trials completed
 # clean up data
 dataStep3<-subset(rawData,subset=accuracy==1) # remove errors
 
-meanRT<-mean(dataStep3$rt)
-sdRT<-sd(dataStep3$rt)
-data<-subset(dataStep3,subset=rt<meanRT+3*sdRT & rt>200) # remove 3 SD outliers
+meanInit<-mean(dataStep3$init_time)
+sdInit<-sd(dataStep3$init_time)
+data<-subset(dataStep3,subset=init_time<meanInit+3*sdInit & rt>200) # remove 3 SD outliers
 attach(data)
 
 # Time measures
@@ -34,3 +34,33 @@ print(model.tables(RT.aov,"means"),digits=6)
 # plot line graph (be sure to execute code at bottom to define "summarySEwithin" function)  Note: error bars defined in Morey 2008
 summary=summarySEwithin(agg,measurevar="rt",withinvars=c("SOAcondition","operation","size","truth"),idvar="subject_nr")
 ggplot(summary,aes(x=SOAcondition,y=rt,shape=operation))+geom_line(aes(group=operation,linetype=operation))+geom_point(size=4)+geom_errorbar(width=0.1,aes(ymin=rt-ci,ymax=rt+ci))+labs(x="SOA condition",y="Mean RT (ms)")+facet_grid(truth~size)+theme(legend.title=element_text(face="bold",size=rel(1.3)),legend.text=element_text(size=rel(1.3)))+theme(axis.title=element_text(face="bold",size=rel(1.3)))+theme(axis.text.x=element_text(size=rel(1.3)))+theme(axis.text.y=element_text(size=rel(1.3)))+theme_classic(20)+theme(axis.line.x=element_line(color="black",size=0.5,linetype="solid"),axis.line.y=element_line(color="black",size=0.5,linetype="solid"))
+
+
+# Time measures
+
+# init
+agg=aggregate(init_time~subject_nr+SOAcondition+operation+size+truth,data=data,FUN="mean") # RT performance data aggregated by subject
+init.aov=aov(init_time~SOAcondition*operation*size*as.factor(truth)+Error(as.factor(subject_nr)/(SOAcondition*operation*size*as.factor(truth))),data=agg)
+summary(init.aov)
+print(model.tables(RT.aov,"means"),digits=6)
+
+# plot line graph (be sure to execute code at bottom to define "summarySEwithin" function)  Note: error bars defined in Morey 2008
+summary=summarySEwithin(agg,measurevar="init_time",withinvars=c("SOAcondition","operation","size","truth"),idvar="subject_nr")
+ggplot(summary,aes(x=SOAcondition,y=init_time,shape=operation))+geom_line(aes(group=operation,linetype=operation))+geom_point(size=4)+geom_errorbar(width=0.1,aes(ymin=init_time-ci,ymax=init_time+ci))+labs(x="SOA condition",y="Mean Initiation Time (ms)")+facet_grid(truth~size)+theme(legend.title=element_text(face="bold",size=rel(1.3)),legend.text=element_text(size=rel(1.3)))+theme(axis.title=element_text(face="bold",size=rel(1.3)))+theme(axis.text.x=element_text(size=rel(1.3)))+theme(axis.text.y=element_text(size=rel(1.3)))+theme_classic(20)+theme(axis.line.x=element_line(color="black",size=0.5,linetype="solid"),axis.line.y=element_line(color="black",size=0.5,linetype="solid"))
+
+
+# Time measures
+
+# MT
+agg=aggregate(rt-init_time~subject_nr+SOAcondition+operation+size+truth,data=data,FUN="mean") # RT performance data aggregated by subject
+names(agg)[6]<-c("MT")
+MT.aov=aov(MT~SOAcondition*operation*size*as.factor(truth)+Error(as.factor(subject_nr)/(SOAcondition*operation*size*as.factor(truth))),data=agg)
+summary(MT.aov)
+print(model.tables(RT.aov,"means"),digits=6)
+
+# plot line graph (be sure to execute code at bottom to define "summarySEwithin" function)  Note: error bars defined in Morey 2008
+summary=summarySEwithin(agg,measurevar="MT",withinvars=c("SOAcondition","operation","size","truth"),idvar="subject_nr")
+ggplot(summary,aes(x=SOAcondition,y=MT,shape=operation))+geom_line(aes(group=operation,linetype=operation))+geom_point(size=4)+geom_errorbar(width=0.1,aes(ymin=MT-ci,ymax=MT+ci))+labs(x="SOA condition",y="Mean MT (ms)")+facet_grid(truth~size)+theme(legend.title=element_text(face="bold",size=rel(1.3)),legend.text=element_text(size=rel(1.3)))+theme(axis.title=element_text(face="bold",size=rel(1.3)))+theme(axis.text.x=element_text(size=rel(1.3)))+theme(axis.text.y=element_text(size=rel(1.3)))+theme_classic(20)+theme(axis.line.x=element_line(color="black",size=0.5,linetype="solid"),axis.line.y=element_line(color="black",size=0.5,linetype="solid"))
+
+
+
